364590b0421065b73edfe0d32e4937463c573f5e
pusong <user0gao@gmail.com>
2025-02-18 19:49:01 +0800

    feat: rewrite the asynchronous read method

--- Parser.Test/Program.cs
+++ Parser.Test/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 namespace Parser.Test
 {
@@ -6,13 +7,23 @@ namespace Parser.Test
     {
         static void Main(string[] args)
         {
+            Stopwatch sw = Stopwatch.StartNew();
             string edfFilePath = @"D:\code\psd_csharp\nunit.test\asserts\X.edf";
 
             using (Reader reader = new Reader(edfFilePath))
             {
-                reader.ReadHeader();
-                double[] buf = reader.ReaderData(0, 501);
-                for (int i = 0; i < buf.Length; i++)
+                double[] buf = new double[1000];
+                sw.Restart();
+
+                int count = reader.ReadDataAsync(27, buf).GetAwaiter().GetResult();
+
+                //double[] buf = reader.ReadFromMemoryMappingFile(edfFilePath, 0, 503);
+
+                sw.Stop();
+
+                Console.WriteLine(sw.ElapsedMilliseconds);
+                Console.WriteLine(count);
+                for (int i = 0; i < count; i++)
                 {
                     Console.WriteLine(buf[i]);
                 }
--- Parser/Reader.cs
+++ Parser/Reader.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace Parser
 {
@@ -15,10 +17,101 @@ namespace Parser
 
     internal class DataReader
     {
-        internal double[] Read(BinaryReader reader, HeaderRecord headerRecord,
-            int signal, int count)
+        private readonly HeaderRecord _headerRecord;
+        private readonly BinaryReader _binaryReader;
+
+        internal DataReader(HeaderRecord headerRecord, BinaryReader binaryReader)
         {
-            int ns = headerRecord.NumberOfSignals.Value;
+            _headerRecord = headerRecord;
+            _binaryReader = binaryReader;
+        }
+
+        private const int _bytesPerSmp = 2;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private long CalculateOffset(int channel)
+        {
+            int ns = _headerRecord.NumberOfSignals.Value;
+            int smpPos = _headerRecord.SamplePos[channel];
+            int smpPerRecord = _headerRecord.NumberOfSamplesInDataRecord.Value[channel];
+            long recordSize = _headerRecord.RecordSize;
+            long bufOffset = _headerRecord.BufOffset[channel];
+
+            // 头部分大小
+            long offset = 256 + ns * 256;
+            // 之前读取到第几块了
+            offset += (smpPos / smpPerRecord) * recordSize;
+            // 该channel在块中偏移多少字节
+            offset += bufOffset;
+            // 从当前块开始偏移了多少字节
+            offset += (smpPos % smpPerRecord) * _bytesPerSmp;
+
+            return offset;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int AdjustCount(int channel, int count)
+        {
+            int smpPos = _headerRecord.SamplePos[channel];
+            int smpPerRecord = _headerRecord.NumberOfSamplesInDataRecord.Value[channel];
+            int smpInFile = smpPerRecord * _headerRecord.NumberOfDataRecords.Value;
+
+            // 如果 count 大于文件内剩余的样本数，则调整 count 为文件剩余的样本数
+            if ((smpPos + count) > smpInFile)
+            {
+                count = smpInFile - smpPos;
+                if (count == 0)
+                    return 0;
+
+                if (count < 0)
+                    throw new ArgumentException("Invalid calculation: the count value became negative.");
+            }
+
+            return count;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private double CalculatePhysicalValue(int channel, short rawVal)
+        {
+            double bitVal = _headerRecord.BitValues[channel];
+            double offsetVal = _headerRecord.Offsets[channel];
+
+            return bitVal * (rawVal + offsetVal);
+        }
+
+        // 方式二
+        //while (currentIndex < count)
+        //{
+        //    // 当前信号的数据块的剩余样本数
+        //    int remainingSamplesInBlock = smpPerRecord - (smpPos % smpPerRecord);
+        //    // 当前批次要读取的样本数：当前块剩余样本数与所需样本数取较小值
+        //    int batchCount = Math.Min(remainingSamplesInBlock, count - currentIndex);
+
+        //    //byte[] dataBatch = _reader.ReadBytes(batchCount * bytesPerSample);
+
+        //    //// 处理读取的数据
+        //    //for (int i = 0; i < batchCount; i++)
+        //    //{
+        //    //    int byteIndex = i * bytesPerSample; // 计算当前样本的字节索引
+        //    //    short rawSample = BitConverter.ToInt16(dataBatch, byteIndex); // 从指定位置解析样本
+        //    //    result[currentIndex++] = _headerRecord.BitValues[channel] * (rawSample + _headerRecord.Offsets[channel]);
+        //    //}
+
+        //    // 更新样本位置
+        //    smpPos += batchCount;
+        //    currentIndex += batchCount;
+
+        //    //// 如果当前块已经读取完，需要跳到下一块的开始位置
+        //    //if (smpPos % smpPerRecord == 0 && currentIndex < count)
+        //    //{
+        //    //    long jump = _headerRecord.RecordSize - (smpPerRecord * bytesPerSample);
+        //    //    _reader.BaseStream.Seek(jump, SeekOrigin.Current); // 跳过当前块的剩余部分
+        //    //}
+        //}
+        internal async Task<int> ReadCore(int signal, double[] buf)
+        {
+            int ns = _headerRecord.NumberOfSignals.Value;
+            int count = buf.Length;
 
             if (signal < 0 || signal >= ns)
                 throw new ArgumentException($"The {nameof(signal)} value is out of the valid range!");
@@ -27,59 +120,51 @@ namespace Parser
                 throw new ArgumentException($"The {nameof(count)} value is invalid!");
 
             if (count == 0)
-                return Array.Empty<double>();
+                return 0;
 
-            int bytesPerSmp = 2;
+            int channel = _headerRecord.MappedSignals[signal];
+            int smpPos = _headerRecord.SamplePos[channel];
+            int smpPerRecord = _headerRecord.NumberOfSamplesInDataRecord.Value[channel];
 
-            int channel = headerRecord.MappedSignals[signal];
-            int smpPos = headerRecord.SamplePos[channel];
-            int smpPerRecord = headerRecord.NumberOfSamplesInDataRecord.Value[channel];
-            int smpInFile = smpPerRecord * headerRecord.NumberOfDataRecords.Value;
+            long offset = CalculateOffset(channel);
+            _binaryReader.BaseStream.Seek(offset, SeekOrigin.Begin);
 
-            // 如果 count 大于文件内剩余的样本数，则调整 count 为文件剩余的样本数
-            if ((smpPos + count) > smpInFile)
-            {
-                count = smpInFile - smpPos;
-                if (count == 0)
-                    return Array.Empty<double>();
+            count = AdjustCount(channel, count);
+            if (count == 0)
+                return 0;
 
-                if (count < 0)
-                    throw new ArgumentException("Invalid calculation: the count value became negative.");
-            }
+            int BATCHSIZE = smpPerRecord;
+            int numBatches = (int)Math.Ceiling((double)count / BATCHSIZE);
 
-            double[] buf = new double[count];
-
-            // 头部分大小
-            long offset = 256 + ns * 256;
-            // 之前读取到第几块了
-            offset += (smpPos / smpPerRecord) * headerRecord.RecordSize;
-            // 该channel在块中偏移多少字节
-            offset += headerRecord.BufOffset[channel];
-            // 从当前块开始偏移了多少字节
-            offset += (smpPos % smpPerRecord) * bytesPerSmp;
-
-            reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+            int currentIndex = 0;
 
             // 假如当前的位置是dataRecord的末尾，需要跳到下一块的开始
-            long jump = headerRecord.RecordSize - (smpPerRecord * bytesPerSmp);
+            long jump = _headerRecord.RecordSize - (smpPerRecord * _bytesPerSmp);
 
-            for (int i = 0; i < count; i++)
+            for (int i = 0; i < numBatches; i++)
             {
-                // 在当前块中读完了该channel的所有samples
-                // 并且需要排除当前指向channel数据块开头的情况
-                if ((smpPos % smpPerRecord == 0) && i > 0)
-                    reader.BaseStream.Seek(jump, SeekOrigin.Current);
+                if (smpPos % smpPerRecord == 0 && i > 0)
+                    _binaryReader.BaseStream.Seek(jump, SeekOrigin.Current);
 
-                byte[] smp = reader.ReadBytes(bytesPerSmp);
-                double val = BitConverter.ToInt16(smp, 0);
-                buf[i] = headerRecord.BitValues[channel] * (val + headerRecord.Offsets[channel]);
+                // 计算当前批次要读取的样本数
+                int batchCount = Math.Min(BATCHSIZE, count - currentIndex);
+                byte[] dataBatch = new byte[batchCount * _bytesPerSmp];
+                await _binaryReader.BaseStream.ReadAsync(dataBatch, 0, batchCount * _bytesPerSmp);
 
-                smpPos++;
+                // 处理读取的数据
+                for (int j = 0; j < batchCount; j++)
+                {
+                    short rawSample = BitConverter.ToInt16(dataBatch, j * _bytesPerSmp);
+                    buf[currentIndex++] = CalculatePhysicalValue(channel, rawSample);
+                }
+
+                smpPos += batchCount;
             }
 
-            headerRecord.SamplePos[channel] = smpPos;
+            int oldPos = _headerRecord.SamplePos[channel];
+            _headerRecord.SamplePos[channel] = smpPos;
 
-            return buf;
+            return smpPos - oldPos;
         }
     }
 
@@ -90,7 +175,7 @@ namespace Parser
         private readonly HeaderReader _headerReader;
         private readonly DataReader _dataReader;
 
-        private HeaderRecord _headerRecord;
+        private readonly HeaderRecord _headerRecord;
         private bool _disposed;
 
         public Reader(Stream stream)
@@ -101,7 +186,8 @@ namespace Parser
                 ?? throw new ArgumentNullException($"{nameof(stream)} is null!");
             _binaryReader = new BinaryReader(stream);
             _headerReader = new HeaderReader();
-            _dataReader = new DataReader();
+            _headerRecord = _headerReader.Read(_binaryReader);
+            _dataReader = new DataReader(_headerRecord, _binaryReader);
         }
 
         public Reader(string edfFilePath)
@@ -123,19 +209,91 @@ namespace Parser
             return File.OpenRead(filePath);
         }
 
-        public HeaderRecord ReadHeader()
-        {
-            _headerRecord = _headerReader.Read(_binaryReader);
-            return _headerRecord;
-        }
+        public async Task<int> ReadDataAsync(int signal, double[] buf)
+            => await _dataReader.ReadCore(signal, buf).ConfigureAwait(false);
 
-        public double[] ReaderData(int signal, int count)
-        {
-            if (_headerRecord == null)
-                ReadHeader();
+        //public double[] ReadFromMemoryMappingFile(string file, int signal, int count)
+        //{
+        //    Dispose();
 
-            return _dataReader.Read(_binaryReader, _headerRecord, signal, count);
-        }
+        //    int ns = _headerRecord.NumberOfSignals.Value;
+
+        //    if (signal < 0 || signal >= ns)
+        //        throw new ArgumentException($"The {nameof(signal)} value is out of the valid range!");
+
+        //    if (count < 0)
+        //        throw new ArgumentException($"The {nameof(count)} value is invalid!");
+
+        //    if (count == 0)
+        //        return Array.Empty<double>();
+
+        //    int bytesPerSmp = 2;
+
+        //    int channel = _headerRecord.MappedSignals[signal];
+        //    int smpPos = _headerRecord.SamplePos[channel];
+        //    int smpPerRecord = _headerRecord.NumberOfSamplesInDataRecord.Value[channel];
+        //    int smpInFile = smpPerRecord * _headerRecord.NumberOfDataRecords.Value;
+
+        //    // 如果 count 大于文件内剩余的样本数，则调整 count 为文件剩余的样本数
+        //    if ((smpPos + count) > smpInFile)
+        //    {
+        //        count = smpInFile - smpPos;
+        //        if (count == 0)
+        //            return Array.Empty<double>();
+
+        //        if (count < 0)
+        //            throw new ArgumentException("Invalid calculation: the count value became negative.");
+        //    }
+
+
+        //    int BATCHSIZE = smpPerRecord;
+        //    int numBatches = (int)Math.Ceiling((double)count / BATCHSIZE);
+
+        //    double[] buf = new double[count];
+        //    int currentIndex = 0;
+
+        //    // 头部分大小
+        //    long offset = 256 + ns * 256;
+        //    // 之前读取到第几块了
+        //    offset += (smpPos / smpPerRecord) * _headerRecord.RecordSize;
+        //    // 该channel在块中偏移多少字节
+        //    offset += _headerRecord.BufOffset[channel];
+        //    // 从当前块开始偏移了多少字节
+        //    offset += (smpPos % smpPerRecord) * bytesPerSmp;
+
+        //    var mmf = MemoryMappedFile.CreateFromFile(file, FileMode.Open);
+
+        //    // 假如当前的位置是dataRecord的末尾，需要跳到下一块的开始
+        //    long jump = _headerRecord.RecordSize - (smpPerRecord * bytesPerSmp);
+
+        //    for (int i = 0; i < numBatches; i++)
+        //    {
+        //        // 每次读取一个批次的数据
+        //        int batchCount = Math.Min(BATCHSIZE, count - currentIndex);
+
+        //        if (smpPos % smpPerRecord == 0 && i > 0)
+        //            offset += jump;
+
+        //        using (var accessor = mmf.CreateViewAccessor(offset, batchCount * bytesPerSmp))
+        //        {
+        //            for (int j = 0; j < batchCount; j++)
+        //            {
+        //                short rawSample = accessor.ReadInt16(j * bytesPerSmp);
+
+        //                buf[currentIndex++] = _headerRecord.BitValues[channel] * (rawSample + _headerRecord.Offsets[channel]);
+        //            }
+        //        }
+
+        //        smpPos += batchCount;
+        //        offset += batchCount * bytesPerSmp;
+        //    }
+
+        //    _headerRecord.SamplePos[channel] = smpPos;
+
+        //    mmf.Dispose();
+
+        //    return buf;
+        //}
 
         public void Dispose()
         {
